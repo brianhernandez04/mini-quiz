6dac79c14062f2da1954a60e907ab6ec5d16e402
Brian Hernandez <hernandezbriannnn@gmail.com>
2022-06-08 22:46:02 -0700

    added the button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,45 @@
 let score = 0;
 let rank;
 const main = document.querySelector('main');
-
+const button = document.querySelector('button');
+button.addEventListener('click', function(){
+    const answer1 = prompt('Who was the first president of the United States?');
+    if (answer1.toUpperCase() === 'GEORGE WASHINGTON' || answer1.toUpperCase() === 'GEORGE WASHINGTON ') {
+    score += 1;
+    }
+    const answer2 = prompt('What is Mexicos national language?');
+    if (answer2.toUpperCase() === 'SPANISH' || answer2.toUpperCase() === 'SPANISH ') {
+    score += 1;
+    }
+    const answer3 = prompt('How many states are in the USA?');
+    if (answer3 == '50' || answer3 == '50 ') {
+    score += 1;
+    }
+    const answer4 = prompt('Where will the world cup take place this year?');
+    if (answer4.toUpperCase() === 'QATAR' || answer4.toUpperCase() === 'QATAR ') {
+    score += 1;
+    }
+    const answer5 = prompt('What year are we currently in?');
+    if (answer5 == '2022' || answer5 == '2022 ') {
+    score += 1;
+    }
+    if (score === 5) {
+        rank = 'Gold'
+    } else if (score === 4 ) {
+        rank = 'Silver'
+    } else if (score >= 1 ) {
+        rank = 'Bronze'
+    } else{
+        rank = 'None :('
+    }
+    main.innerHTML = 
+    `<h2 style="color: white; text-align: center; padding: 40vh 20vw 0 20vw;"> You got ${score}/5 correct!</h2>
+     <p style="color: white; text-align: center;">Crown Earned: <strong>${rank}</strong></p>`
+;
+    });
+    
+    
+/*
 const answer1 = prompt('Who was the first president of the United States?');
 if (answer1.toUpperCase() === 'GEORGE WASHINGTON' || answer1.toUpperCase() === 'GEORGE WASHINGTON ') {
     score += 1;
@@ -36,3 +74,4 @@ main.innerHTML =
     `<h2 style="color: white; text-align: center; padding: 40vh 20vw 0 20vw;"> You got ${score}/5 correct!</h2>
      <p style="color: white; text-align: center;">Crown Earned: <strong>${rank}</strong></p>`
 ;
+*/
